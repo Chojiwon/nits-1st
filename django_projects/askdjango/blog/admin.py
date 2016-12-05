48b980ce0b88b078350fba74d1212194aea93bef
Chinseok Lee <allieuslee@gmail.com>
2016-12-05 16:21:25 +0900

    blog.models.Post 모델에 대해서 커스텀 ModelAdmin 적용

--- django_projects/askdjango/blog/admin.py
+++ django_projects/askdjango/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'get_content_size', 'created_at', 'updated_at']
+    search_fields = ['title']
+    list_filter = ['created_at', 'updated_at']
+
+    def get_content_size(self, post):
+        return '{} 글자'.format(len(post.content))
+
+
+admin.site.register(Post, PostAdmin)
+
