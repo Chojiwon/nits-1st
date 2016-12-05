553d70c88ab167aa994f4edca6910d288d28e963
Chinseok Lee <allieuslee@gmail.com>
2016-12-05 15:07:40 +0900

    blog.models.Post 모델에 tags 필드 추가 및 마이그레이션

--- django_projects/askdjango/blog/models.py
+++ django_projects/askdjango/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    tags = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
