6d7eab0c0ad9c31104cb87e1f28735c46f286aa9
Chinseok Lee <allieuslee@gmail.com>
2016-12-08 15:29:18 +0900

    blog.models.Tag 모델의 name 필드에 unique=True 속성을 적용

--- django_projects/askdjango/blog/models.py
+++ django_projects/askdjango/blog/models.py
@@ -22,7 +22,7 @@ class Comment(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.name
