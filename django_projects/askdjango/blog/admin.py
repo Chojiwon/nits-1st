9a113977febfbc1481405f42522c40c96f643d3c
Chinseok Lee <allieuslee@gmail.com>
2016-12-08 14:24:33 +0900

    Post 모델과 1:N의 관계에 있는 blog.models.Comment 모델을 생성하고, 마이그레이션 적용

--- django_projects/askdjango/blog/admin.py
+++ django_projects/askdjango/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -21,4 +21,7 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+
+
+admin.site.register(Comment)
 
--- django_projects/askdjango/blog/models.py
+++ django_projects/askdjango/blog/models.py
@@ -12,3 +12,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
