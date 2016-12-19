37a0531cffc35dfcf18af75c1d2d6d73f1ca59d0
Chinseok Lee <allieuslee@gmail.com>
2016-12-19 16:31:53 +0900

    blog.models.Post 모델에 photo ImageField 추가

--- django_projects/askdjango/blog/models.py
+++ django_projects/askdjango/blog/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     content = models.TextField()
+    photo = models.ImageField(blank=True)
     author = models.CharField(max_length=20)
     tags = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
