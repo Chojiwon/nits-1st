460a3987e6e72e4a409bc9813ff2d7ee61fca080
Chinseok Lee <allieuslee@gmail.com>
2016-12-19 16:13:17 +0900

    STATIC 파일 설정 (STATICFILES_DIRS, STATIC_ROOT)

--- django_projects/askdjango/askdjango/settings.py
+++ django_projects/askdjango/askdjango/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askdjango', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 INTERNAL_IPS = ['127.0.0.1']
 
