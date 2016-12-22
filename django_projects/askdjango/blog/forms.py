3f4e651563271bf71f4d1405b8617de225bc6b4a
Chinseok Lee <allieuslee@gmail.com>
2016-12-22 14:15:14 +0900

    blog.forms.PostModelForm 의 title 필드에 대해 clean_title Validator 적용

--- django_projects/askdjango/blog/forms.py
+++ django_projects/askdjango/blog/forms.py
@@ -19,6 +19,14 @@ class PostModelForm(forms.ModelForm):
         # fields = '__all__'
         fields = ['title', 'content', 'photo']
 
+    def clean_title(self):
+        title = self.cleaned_data.get('title', None)
+        if title:
+            if len(title) % 2 == 0:
+                raise ValidationError('제목은 홀수로 입력하세요.')
+            title = title.strip()  # 좌우 화이트 스페이스를 제거
+        return title
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
