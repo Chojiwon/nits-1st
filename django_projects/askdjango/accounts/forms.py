d0bb284c69e5bda3d311d19ed466d4e33e6a4906
Chinseok Lee <allieuslee@gmail.com>
2016-12-26 15:54:07 +0900

    회원가입시에 SignupForm 을 통해 email, first_name, last_name 입력

---
+++ django_projects/askdjango/accounts/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = ('username', 'email', 'last_name', 'first_name')
+
