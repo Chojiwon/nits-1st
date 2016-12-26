29c3ae4b08670589071a699fba24b907e0896224
Chinseok Lee <allieuslee@gmail.com>
2016-12-26 16:15:08 +0900

    회원가입시에 휴대폰번호, 주소를 입력받도록 함.

--- django_projects/askdjango/accounts/forms.py
+++ django_projects/askdjango/accounts/forms.py
@@ -1,7 +1,29 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from accounts.models import Profile
 
 
 class SignupForm(UserCreationForm):
+    phone = forms.CharField(label='휴대폰번호')
+    address = forms.CharField()
+
     class Meta(UserCreationForm.Meta):
         fields = ('username', 'email', 'last_name', 'first_name')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.save()
+
+        phone = self.cleaned_data.get('phone')
+        address = self.cleaned_data.get('address')
+
+        try:
+            # Profile.objects.get(user=user) 와 동일
+            user.profile.phone = phone
+            user.profile.address = address
+            user.profile.save()
+        except Profile.DoesNotExist:
+            Profile.objects.create(user=user, phone=phone, address=address)
+
+        return user
 
