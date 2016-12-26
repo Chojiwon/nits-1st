d7ffeae6c5846ad6e327c16f4df75fb7a1bd80e8
Chinseok Lee <allieuslee@gmail.com>
2016-12-26 16:29:47 +0900

    로그인 기능 추가

--- django_projects/askdjango/accounts/urls.py
+++ django_projects/askdjango/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', auth_views.login, name='login', kwargs={
+        'template_name': 'accounts/login_form.html',
+    }),
     url(r'^profile/$', views.profile, name='profile'),
 ]
 
