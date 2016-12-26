f4f5a704352848a9915fa4c18d5ec81bed5294bc
Chinseok Lee <allieuslee@gmail.com>
2016-12-26 15:26:05 +0900

    accounts.views.signup 을 통해 회원가입 구현

--- django_projects/askdjango/accounts/urls.py
+++ django_projects/askdjango/accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^profile/$', views.profile, name='profile'),
 ]
 
--- django_projects/askdjango/accounts/views.py
+++ django_projects/askdjango/accounts/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
 
 
 def profile(request):
