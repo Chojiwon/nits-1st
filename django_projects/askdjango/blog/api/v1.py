3500542b1971ed39e1531fc1af9af1de35c9653d
Chinseok Lee <allieuslee@gmail.com>
2016-12-29 15:46:45 +0900

    api v1 뷰에 JsonResponse 활용

--- django_projects/askdjango/blog/api/v1.py
+++ django_projects/askdjango/blog/api/v1.py
@@ -1,6 +1,5 @@
-import json
 from django.conf.urls import url
-from django.http import HttpResponse
+from django.http import JsonResponse
 from blog.models import Post
 
 
@@ -12,8 +11,10 @@ def post_list(request):
         for post in qs
     ]
 
-    json_string = json.dumps(post_list, indent=4, ensure_ascii=False)
-    return HttpResponse(json_string)
+    return JsonResponse(post_list, safe=False, json_dumps_params={
+        'indent': 4,
+        'ensure_ascii': False,
+    })
 
 
 urlpatterns = [
