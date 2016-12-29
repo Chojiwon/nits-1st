aea2338ca7b962f899994c3b8ce8e1a3614e5f83
Chinseok Lee <allieuslee@gmail.com>
2016-12-29 15:27:50 +0900

    blog api v1 post_list 에서 Post 목록 노출

--- django_projects/askdjango/blog/api/v1.py
+++ django_projects/askdjango/blog/api/v1.py
@@ -1,10 +1,18 @@
 import json
 from django.conf.urls import url
 from django.http import HttpResponse
+from blog.models import Post
 
 
 def post_list(request):
-    json_string = json.dumps([1, 2, 3, 4], indent=4)
+    qs = Post.objects.all()
+
+    post_list = [
+        {'id': post.id, 'title': post.title}
+        for post in qs
+    ]
+
+    json_string = json.dumps(post_list, indent=4, ensure_ascii=False)
     return HttpResponse(json_string)
 
 
