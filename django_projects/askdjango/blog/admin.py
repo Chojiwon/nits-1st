c959b2f4620b03fc93cd072b4e1f13fdef216a5b
Chinseok Lee <allieuslee@gmail.com>
2016-12-29 14:26:02 +0900

    PostAdmin 에 list_display, list_display_links 옵션 적용

--- django_projects/askdjango/blog/admin.py
+++ django_projects/askdjango/blog/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from blog.models import Post, Comment, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'get_content_size', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'link', 'get_content_size', 'created_at', 'updated_at']
+    list_display_links = ['title', 'created_at']
+    # list_editable = ['title']
     search_fields = ['title']
     list_filter = ['created_at', 'updated_at']
     actions = ['send_push']
+
+    def link(self, post):
+        return format_html('<a href="http://naver.com">라라랜드 실시간 보기</a>')
 
     def get_content_size(self, post):
         return '{} 글자'.format(len(post.content))
