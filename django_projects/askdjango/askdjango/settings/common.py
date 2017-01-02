8199197ccbbb9005d14cf9574582402469386122
Chinseok Lee <allieuslee@gmail.com>
2017-01-02 18:11:07 +0900

    서버 배포

--- django_projects/askdjango/askdjango/settings/common.py
+++ django_projects/askdjango/askdjango/settings/common.py
@@ -162,19 +162,6 @@ CELERY_BROKER_URL = 'amqp://'
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 
 
-import os
-import raven
-
-GIT_ROOT = os.path.join(BASE_DIR, '..', '..')  # FIXME: 프로젝트에 맞춰 수정
-
-RAVEN_CONFIG = {
-    # FIXME: 각자 설정에 맞춰 수정
-    # https://docs.sentry.io/clients/python/integrations/django/
-    'dsn': 'https://c1647f59f36e44f4833d5c6626695cd1:392001118d9d4e00b9dd0227bde8ede5@sentry.io/124899',
-    # If you are using git, you can also automatically configure the release based on the git info.
-    'release': raven.fetch_git_sha(GIT_ROOT),
-}
-
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'asgi_ipc.IPCChannelLayer',
--- django_projects/askdjango/askdjango/settings/prod.py
+++ django_projects/askdjango/askdjango/settings/prod.py
@@ -29,3 +29,14 @@ CHANNEL_LAYERS = {
     },
 }
 
+
+import os
+import raven
+
+RAVEN_CONFIG = {
+    # FIXME: 각자 설정에 맞춰 수정
+    # https://docs.sentry.io/clients/python/integrations/django/
+    'dsn': 'https://c1647f59f36e44f4833d5c6626695cd1:392001118d9d4e00b9dd0227bde8ede5@sentry.io/124899',
+    # If you are using git, you can also automatically configure the release based on the git info.
+    'release': 'askdjango',
+}
