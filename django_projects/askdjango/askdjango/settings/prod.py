b25089ab60f42b18add5d93ab34cb4d8e0b2cd76
Chinseok Lee <allieuslee@gmail.com>
2017-01-02 18:05:01 +0900

    빠진 reqs 팩키지 추가

--- django_projects/askdjango/askdjango/settings/prod.py
+++ django_projects/askdjango/askdjango/settings/prod.py
@@ -19,3 +19,13 @@ DATABASES = {
     },
 }
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('localhost', 6379)],
+            },
+        'ROUTING': 'askdjango.routing.channel_routing',
+    },
+}
+
